d25384ec408e23f4882903f20bf754bd65640a44
Spencer Feng <spencer.feng@icloud.com>
2021-03-23 15:23:03 +1100

    feat: create SelectFaceImageTabelViewCell

---
+++ FaceStickerMaker/Views/SelectFaceImageTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  SelectFaceImageTableViewCell.swift
+//  FaceStickerMaker
+//
+//  Created by Spencer Feng on 23/3/21.
+//
+
+import Foundation
+import UIKit
+
+class SelectFaceImageTableViewCell: UITableViewCell {
+    
+    // MARK: - Initializers
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
