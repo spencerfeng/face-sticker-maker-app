3c53fb330bfc6383ec2718388ea9c764be5eaf3e
Spencer Feng <spencer.feng@icloud.com>
2021-03-23 11:28:36 +1100

    feat: add ChooseCroppedImagesViewModel

---
+++ FaceStickerMaker/ViewModels/ChooseCroppedImagesViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ChooseCroppedImagesViewModel.swift
+//  FaceStickerMaker
+//
+//  Created by Spencer Feng on 23/3/21.
+//
+
+import Foundation
+
+class ChooseCroppedImagesViewModel {
+    
+    // MARK: - Output
+    @Published private(set) var croppedImages = [FaceImage]()
+    
+}
