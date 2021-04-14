294558898ca3bfba10e623e0a41e08ef6d9e06dc
Spencer Feng <spencer.feng@icloud.com>
2021-04-14 11:52:31 +1000

    feat: add a helper function to resize image

--- FaceStickerMaker/Helper.swift
+++ FaceStickerMaker/Helper.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 class Helper {
     static func filePath(forKey key: String, forFormat format: String) -> URL? {
@@ -14,5 +15,17 @@ class Helper {
                                                  in: FileManager.SearchPathDomainMask.userDomainMask).first else { return nil }
             
         return documentURL.appendingPathComponent("\(key).\(format)")
+    }
+    
+    static func resizeImage(image: CGImage, size: CGSize, radius: CGFloat = 0, orientation: UIImage.Orientation = .up) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        let resizedImage = renderer.image { _ in
+            let rect = CGRect(origin: .zero, size: size)
+            let rounded = UIBezierPath(roundedRect: rect, cornerRadius: radius)
+            rounded.addClip()
+            UIImage(cgImage: image, scale: 1, orientation: orientation).draw(in: rect)
+        }
+            
+        return resizedImage
     }
 }
