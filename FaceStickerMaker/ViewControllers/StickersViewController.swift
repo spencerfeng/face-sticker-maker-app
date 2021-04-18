883b37e27d8e0fd34a9dd96c173c387f7f0a8a62
Spencer Feng <spencer.feng@icloud.com>
2021-04-18 23:05:00 +1000

    fix: remove the height constraint for the top navigation bar for sticker screen

--- FaceStickerMaker/ViewControllers/StickersViewController.swift
+++ FaceStickerMaker/ViewControllers/StickersViewController.swift
@@ -136,7 +136,6 @@ class StickersViewController: UIViewController {
             topNavigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             topNavigationBar.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 1.0),
             topNavigationBar.widthAnchor.constraint(equalTo: view.widthAnchor),
-            topNavigationBar.heightAnchor.constraint(equalToConstant: 44.0),
             // note: we do not need to set the height of topNavigationBar explicitly, since it has its inherent value
             
             stickersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
